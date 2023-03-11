50a3c71ad8732d3a9018df9fcf32c92c0afd1f6f
Yang Xiaozai <y.jinbiao@foxmail.com>
2023-03-11 22:00:05 +0800

    ✨ feat(vite别名): 新增/assets路径配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 //引入自动引入插件
@@ -17,6 +17,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "/assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-})
+});
