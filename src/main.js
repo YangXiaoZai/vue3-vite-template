fe1e7d96cde7705a84cadfef8fd17c4fbcd963f4
Yang Xiaozai <y.jinbiao@foxmail.com>
2023-03-11 09:04:49 +0800

    🐞 fix(main.js): 修复antd样式不生效

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 import Antd from 'ant-design-vue';
 
-import './style.css' //公用css
+import 'ant-design-vue/dist/antd.css'; // antd样式
 import "normalize.css/normalize.css"; // CSS初始化
 import "tailwindcss/tailwind.css"; //tailwind插件
-import 'ant-design-vue/dist/antd.css'; // antd样式
+import './style.css'; //公用css
 
 
 createApp(App).use(Antd).mount('#app');
