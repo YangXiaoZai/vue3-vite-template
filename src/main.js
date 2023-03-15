085dfee1cfb21405bf2ceda0061bfb8e62c7065f
Yang Xiaozai <y.jinbiao@foxmail.com>
2023-03-15 18:00:23 +0800

    ✨ feat(公用less): 新增公用less文件夹

--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ import "normalize.css/normalize.css"; // CSS初始化
 import "tailwindcss/tailwind.css"; //tailwind插件
 import "ant-design-vue/dist/antd.less"; // antd样式
 
-import "./style.css"; //公用css
+import "./style/common.less"; //公用css
 
 createApp(App).use(router).use(Antd).mount("#app");
